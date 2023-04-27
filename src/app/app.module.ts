c0f2212e9842dcf80ebf47611f92ebce3b0926dc
leandrodias95 <leandrogit95@gmail.com>
2023-04-26 23:36:07 -0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
-    BrowserModul,
+    BrowserModule,
     MatSlideToggleModule,
     MatButtonModule,
     MatDividerModule,
