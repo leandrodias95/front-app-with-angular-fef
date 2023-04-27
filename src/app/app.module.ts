68fff996e23d53847d322f09ad1a9d4631885301
leandrodias95 <leandrogit95@gmail.com>
2023-04-26 23:34:19 -0300

    mensagem de exemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModul,
     MatSlideToggleModule,
     MatButtonModule,
     MatDividerModule,
